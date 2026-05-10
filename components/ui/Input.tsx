4387ac6cb326da4abb23227f57a07bd3e23ac2df
Piyushyadav <yadavpiyush045@gmail.com>
2026-05-10 12:50:11 +0530

    fix: extend Input to accept all native HTML input props (onKeyDown, minLength, etc)

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -1,45 +1,17 @@
 import React from "react";
 
-interface InputProps {
-  type?: string;
-  placeholder?: string;
-  value?: string;
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  id?: string;
-  name?: string;
-  autoComplete?: string;
-  required?: boolean;
-  className?: string;
-  disabled?: boolean;
-  maxLength?: number;
-}
+type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
 
-export const Input: React.FC<InputProps> = ({
-  type = "text",
-  placeholder,
-  value,
-  onChange,
-  id,
-  name,
-  autoComplete,
-  required,
-  className = "",
-  disabled = false,
-  maxLength,
-}) => {
-  return (
-    <input
-      id={id}
-      name={name}
-      type={type}
-      autoComplete={autoComplete}
-      required={required}
-      placeholder={placeholder}
-      value={value}
-      onChange={onChange}
-      disabled={disabled}
-      maxLength={maxLength}
-      className={`border p-2 rounded w-full ${className}`}
-    />
-  );
-};
+export const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className = "", ...props }, ref) => {
+    return (
+      <input
+        ref={ref}
+        className={`border p-2 rounded w-full ${className}`}
+        {...props}
+      />
+    );
+  }
+);
+
+Input.displayName = "Input";
